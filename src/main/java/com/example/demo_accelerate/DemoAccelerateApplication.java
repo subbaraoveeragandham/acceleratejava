c60a049d29062f96c2e9af682ef5142e408c5a67
subbaraoveeragnadham <venkatasubbarao9493@gmail.com>
2020-02-16 12:46:34 +0530

    sample java program

---
+++ src/main/java/com/example/demo_accelerate/DemoAccelerateApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo_accelerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAccelerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAccelerateApplication.class, args);
+		System.out.println("helloworld");
+	}
+	
+
+}
---
+++ src/test/java/com/example/demo_accelerate/DemoAccelerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_accelerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAccelerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
